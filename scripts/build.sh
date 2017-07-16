809fefa6f11e086dcbb6cb191044025b580d1425
Ian Donaldson <ian@exidy.org>
2017-07-16 22:07:04 +1000

    Commit simple caching strategy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
-docker  run -v `pwd`:/work \
+BUILD_DIR=$(pwd)
+
+echo ":s3: Seeding cache"
+
+mkdir -f ${BUILD_DIR}/cache
+aws s3 sync s3://dius-dev-build-cache/${BUILDKITE_PIPELINE_SLUG}/caches ${BUILD_DIR}/cache
+
+echo ":hammer: Building app"
+
+docker run \
+        -v ${BUILD_DIR}:/work \
+        -v ${BUILD_DIR}/cache:/root/.m2 \
         -w /work \
         openjdk:8 ./mvnw package
+
+echo ":s3: Syncing cache"
+aws s3 sync --delete ${BUILD_DIR}/cache s3://dius-dev-build-cache/${BUILDKITE_PIPELINE_SLUG}/caches 
